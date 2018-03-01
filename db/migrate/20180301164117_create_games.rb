be77bb5e4a64a028b54fa083b8f65bef87f81061
saurookadook <maskiella@gmail.com>
2018-03-01 11:59:37 -0500

    Finished filling in migrations

--- db/migrate/20180301164117_create_games.rb
+++ db/migrate/20180301164117_create_games.rb
@@ -2,6 +2,7 @@ class CreateGames < ActiveRecord::Migration[5.1]
   def change
     create_table :games do |t|
       t.string :name
+      t.integer :genre_id
     end
   end
 end
--- db/migrate/20180301164158_create_user_games.rb
+++ db/migrate/20180301164158_create_user_games.rb
@@ -1,4 +1,9 @@
 class CreateUserGames < ActiveRecord::Migration[5.1]
   def change
+    create_table :user_games do |t|
+      t.integer :user_id
+      t.integer :game_id
+      t.integer :platform_id
+    end
   end
 end
